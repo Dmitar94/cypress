7be5bda5a0e6d1ef4b143eb19fb9821c006bad1c
milanzivanovns <sremac1994@gmail.com>
2024-01-26 21:43:03 +0100

    ovo je samo pocetak

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,16 +1,26 @@
+/// <reference types="Cypress" />
+
 describe("Login functionality - Gallery App", () => {
-    it("Unsuccessful login -invalid email address", () => {
-        cy.visit("https://gallery-app.vivifyideas.com/login");
-
-        cy.get("#email").type("sremac1994@gmail.com");
-        cy.get("password").type("Dmitarsremcic1994");
-
-
-    })
+})
+it("Successfull login" , ()=>{
+    cy.visit("login"); 
+    cy.url().should('contain',"login")
+    cy.get("#email").type("peraperic12345@gmail.com");
+    cy.get("#password").type("pera12345");
+    cy.get("button").click();
+    cy.get("h1").should("have.text", "Please login")
+    cy.get("label").eq(0).should("have.text","Email")
+    cy.get("label").eq(1).should("have.text","Password")
+    cy.get("a").contains('Logout').click();
     
-    
 
+    //cy.get('a[role="button "]').click();
+    
 });
+
+    it.only("Unsuccessful registration -invalid email adress", () => {
+        cy.visit("/register");
+    })
 
 
  
--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,8 +1,18 @@
 ///  <reference types="Cypress" />
 
-describe("Succsesfull login ", () => {
-    cy.visit("https://gallery-app.vivifyideas.com/login");
-
+describe("Succsesfull register functionality - Gallery App" , () => {
+    // cy.visit("https://gallery-app.vivifyideas.com/login");
+    it.only("Unsuccessful registration -invalid email adress", () => {
+        cy.visit("/register");
+        cy.url().should('contain', '/register');
+        
+        
+        cy.get("#first-name").type("Dmimi")
+        cy.get("#last-name").type("Cic")
+        cy.get("#email").type("tarmiricmi@gmil.com")
+        cy.get("#password").type("12345678")
+        cy.get("#password-confirmation").type("12345678")
+        cy.get("#type=checkbox").check()
+    })
     
-}
-
+})
